ca9ed7b84bcc381b41926290735f5e6074a303f3
Alfredo Pantoja <chololoy@gmail.com>
2013-09-08 15:17:39 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+AngularCasts::Application.config.secret_key_base = '898449aa752e1967adea2a40a74ddebe8e22a060c3264355b5053fdcb3c8b04bc1795c0dc2fbd346b84f26b304502f2836a9a92ae3dfc0460ed20af7d2732761'
